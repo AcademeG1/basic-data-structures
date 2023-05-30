6122c2ee9fa50cefc4ac50b16f68a41e677d4780
Michail <36785100+AcademeG1@users.noreply.github.com>
2023-05-30 22:16:32 +0300

    feat: add implementation a queue using a class

--- queue.js
+++ queue.js
@@ -1,6 +1,38 @@
-const queue = [];
+// const queue = [];
 
-queue.unshift(1);
-queue.unshift(2);
+// queue.unshift(1);
+// queue.unshift(2);
 
-queue.shift();
+// queue.shift();
+
+class Queue {
+    constructor() {
+        this.head = 0;
+        this.tail = 0;
+        this.storage = {};
+    }
+
+    getLength() {
+        return this.tail - this.head;
+    }
+
+    shift() {
+        if (this.head < this.tail) {
+            const data = this.storage[this.head];
+
+            delete this.storage[this.head];
+            this.head++;
+            return data;
+        } else {
+            console.log('Очередь пуста');
+        }
+    }
+
+    unshift(value) {
+        this.storage[this.tail] = value;
+        this.tail++; // указывает на следующее пустое место
+    }
+
+}
+
+const que = new Queue();
