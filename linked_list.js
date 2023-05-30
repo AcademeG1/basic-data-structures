a68a4a7a0c67279d921dac8050c07f98b1a7b15c
Michail <36785100+AcademeG1@users.noreply.github.com>
2023-05-30 23:50:01 +0300

    feat: add methods insertAfter, delete

--- linked_list.js
+++ linked_list.js
@@ -11,7 +11,7 @@ class LinkedList {
         this.tail = null;
     }
 
-    push(value) { // добавление в конец
+    push (value) { // добавление в конец
         const node = new LinkedListNode(value);
 
         if (!this.head || !this.tail) {
@@ -27,12 +27,80 @@ class LinkedList {
         return this;
     }
 
-    prepend(value) { // добавление в head
+    prepend (value) { // добавление в head
         const node = new LinkedListNode(value, this.head);
         
         this.head = node;
 
         if (!this.tail) {
+            this.tail = node;
+        }
+
+        return this;
+    }
+
+    find (value) {
+
+        if (!this.head) {
+            return null;
+        }
+
+        let currentNode = this.head;
+
+        while(currentNode) {
+            if (currentNode.value === value) {
+                return currentNode;
+            }
+
+            currentNode = currentNode.nextLink;
+        }
+
+        return null;
+    }
+
+    delete (value) {
+        if (!this.head) {
+            return null;
+        }
+
+        let deletedNode = null;
+
+        while(this.head && this.head.value === value) {
+            deletedNode = this.head;
+
+            this.head = this.head.nextLink;
+        }
+
+        let currentNode = this.head;
+
+        if (currentNode != null) {
+            while (currentNode.nextLink) {
+                if (currentNode.nextLink.value === value) {
+                    deletedNode = currentNode.nextLink;
+                    currentNode.nextLink = currentNode.nextLink.nextLink;
+                } else {
+                    currentNode = currentNode.nextLink;
+                }
+            }
+        }
+
+        if (this.tail.value === value) {
+            this.tail = currentNode;
+        }
+
+        return this;
+    }
+
+    insertAfter (value, prevNode) {
+        if (prevNode === null) {
+            return this;
+        }
+
+        const node = new LinkedListNode(value);
+        node.nextLink = prevNode.nextLink;
+        prevNode.nextLink = node;
+        
+        if (node.nextLink === null) {
             this.tail = node;
         }
 
