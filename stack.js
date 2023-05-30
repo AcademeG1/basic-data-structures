0583485446df8b5138b9d7b1ce97562b3114b377
Michail <36785100+AcademeG1@users.noreply.github.com>
2023-05-30 22:31:16 +0300

    feat: add implementation a stack using a class

--- stack.js
+++ stack.js
@@ -1,4 +1,44 @@
-const stack = [];
+// const stack = [];
+
+// stack.push(1);
+// stack.pop();
+
+class Stack {
+    constructor() {
+        this.size = 0;
+        this.storage = {};
+    }
+
+    push(value) {
+        this.storage[this.size] = value;
+        this.size++;
+    }
+
+    pop() {
+        if (this.size > 0) {
+            this.size--;
+            const data = this.storage[this.size];
+            delete this.storage[this.size];
+            return data;
+        }
+    }
+
+    getLength() {
+        return this.size - 1;
+    }
+
+}
+
+const stack = new Stack();
 
 stack.push(1);
-stack.pop();
+stack.push(2);
+stack.push(3);
+
+console.log(stack.getLength());
+console.log(stack.getLength());
+console.log(stack.getLength());
+
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
